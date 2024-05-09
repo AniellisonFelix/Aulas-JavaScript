7f959624c9662a98162a42e8f53fb753e4c92f1c
Aniellison Felix <aniellisom@gmail.com>
2024-05-09 10:27:39 -0300

    adicinando aulas js

---
+++ aulajs11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/h`);
+if (vel > 60) { // condiçao simples
+	console.log('Você ultrapassou o limite de velocidade, multado!');
+}
+console.log('Dirija sempre usando cinto de segurança!');
---
+++ aulajs11/exe009.js
@@ -0,0 +1,6 @@
+var pais = 'frança'
+if (pais == 'Brasil') {
+	console.log('Você é Brasileiro');
+} else {
+	console.log('Você é estrangeiro!');
+}
